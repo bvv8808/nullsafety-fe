fe1e66fcc908b1646c49ea176d21c02e5ef6f439
김현우 <kimhyeonwoo@Hyeonwoo-iMac.local>
2021-10-11 04:11:25 +0900

    1010 메인 화면 작업 끝

--- src/Screens/Main/index.tsx
+++ src/Screens/Main/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { api_getCategoryNames, getMainData } from "../../lib/fetcher";
 import { TMainData } from "../../lib/types";
@@ -11,10 +11,24 @@ const shuckMainData = {
   contentPreviewsByLike: [],
 };
 
+// 컨텐츠 목록 스크롤을 위한 변수
+// (1) wrapper mousedown 시 startX 할당
+// (2) wrapper mouseup 시 e.screenX와 startX를 비교하여 스크롤 여부 판단
+// (3) 스크롤 이후라면 dismissClick = true;
+// (4) container의 onClick 핸들러에서 dismissClick===true라면 false로 재할당 후 핸들러 종료
+let dismissClick = false;
+let startX = 0;
+let scrollLeft = 0;
+let isMouseDown = false;
+
 const MainScreen = () => {
   const [categories, setCategories] = useState<string[]>([]);
   const [mainData, setMainData] = useState<TMainData>(shuckMainData);
   const [selectedFilter, setSelectedFilter] = useState<"hit" | "like">("hit");
+
+  const refTabHit = useRef<HTMLDivElement>(null);
+  const refTabLike = useRef<HTMLDivElement>(null);
+  const refContentsWrapper = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     getMainData().then((r) => {
@@ -30,12 +44,47 @@ const MainScreen = () => {
     radioHit.checked = true;
   }, []);
 
+  useEffect(() => {
+    if (!refTabHit.current || !refTabLike.current) return;
+
+    const targetClass = "main-tab-active";
+    if (selectedFilter === "hit") {
+      refTabHit.current.classList.add(targetClass);
+      refTabLike.current.classList.remove(targetClass);
+    } else {
+      refTabHit.current.classList.remove(targetClass);
+      refTabLike.current.classList.add(targetClass);
+    }
+  }, [selectedFilter]);
+
   const switchToHit = useCallback(() => {
     setSelectedFilter("hit");
   }, []);
 
   const switchToLike = useCallback(() => {
     setSelectedFilter("like");
+  }, []);
+
+  const _onMouseDown = useCallback((e) => {
+    if (!refContentsWrapper.current) return;
+
+    isMouseDown = true;
+    scrollLeft = refContentsWrapper.current.scrollLeft;
+    startX = e.pageX;
+  }, []);
+
+  const _onMouseMove = useCallback((e) => {
+    if (!isMouseDown || !refContentsWrapper.current) return;
+    const d = startX - e.pageX;
+
+    refContentsWrapper.current.scrollTo(scrollLeft + d, 0);
+  }, []);
+
+  const _onMouseUp = useCallback((e) => {
+    if (e.pageX === startX) dismissClick = false;
+    else dismissClick = true;
+
+    isMouseDown = false;
   }, []);
 
   return (
@@ -75,34 +124,60 @@ const MainScreen = () => {
 
       {/* @@ 768px 이하일 때 렌더링 */}
       <div className="main-tab-container">
-        <div className="main-tab" onClick={switchToHit}>
+        <div ref={refTabHit} className="main-tab" onClick={switchToHit}>
           조회순
         </div>
-        <div className="main-tab" onClick={switchToLike}>
+        <div ref={refTabLike} className="main-tab" onClick={switchToLike}>
           좋아요순
         </div>
       </div>
 
-      <div className="main-contents-wrapper">
+      <div
+        ref={refContentsWrapper}
+        className="main-contents-wrapper"
+        onMouseDown={_onMouseDown}
+        onMouseMove={_onMouseMove}
+        onMouseUp={_onMouseUp}
+      >
         {mainData[
           selectedFilter === "hit"
             ? "contentPreviewsByHit"
             : "contentPreviewsByLike"
         ].map((c) => {
-          return <div key={c.id}>{c.id}</div>;
+          return (
+            <div
+              className="main-content-container"
+              key={c.id}
+              onClick={() => {
+                if (dismissClick) {
+                  dismissClick = false;
+                  return;
+                }
+
+                // do something
+                console.log("clicked");
+              }}
+            >
+              {c.id}
+            </div>
+          );
         })}
       </div>
 
-      <span className="main-text-hot">카테고리</span>
+      <div className="main-text-container">
+        <span className="main-text-hot">카테고리</span>
+      </div>
 
       <div className="main-category-wrapper">
         {/* Grid Layout */}
         {categories.map((c) => (
-          <div className="main-category" key={c}>
-            <Link className="main-category-link" to={`/contents?category=${c}`}>
-              {c}
-            </Link>
-          </div>
+          <Link
+            key={c}
+            className="main-category-link"
+            to={`/contents?category=${c}`}
+          >
+            {c}
+          </Link>
         ))}
       </div>
     </div>
